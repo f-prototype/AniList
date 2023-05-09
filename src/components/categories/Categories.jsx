a6957b146d629463c8ef7a1412057102f669872e
Retover <lehasazonoff@mail.ru>
2023-05-09 18:28:05 +0300

    fix adaptive filters

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,4 +1,5 @@
 import { Filters } from '../filters/Filters';
+
 import { Sort } from '../sort/Sort';
 import styles from './Categories.module.css';
 export const Categories = () => {
---
+++ src/utils/seasons.js
@@ -0,0 +1,6 @@
+export const seasons = [
+  { attributes: { slug: 'winter', title: 'winter' } },
+  { attributes: { slug: 'spring', title: 'spring' } },
+  { attributes: { slug: 'summer', title: 'summer' } },
+  { attributes: { slug: 'fall', title: 'fall' } },
+];
